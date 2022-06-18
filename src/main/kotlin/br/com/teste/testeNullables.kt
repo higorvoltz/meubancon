d2ca30b0b84b38c7aa94e0367829deb0fd264eb1
higorvoltz <higor.amorim@voltzmotors.com>
2022-06-18 09:23:03 -0300

    nullables

---
+++ src/main/kotlin/br/com/teste/testeNullables.kt
@@ -0,0 +1,21 @@
+package br.com.teste
+
+import br.com.modelo.Endereco
+import java.util.*
+
+fun testeNullables() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua X")
+    println(enderecoNulo?.logradouro?.length)
+    enderecoNulo?.let { println(it.logradouro.uppercase(Locale.getDefault())) }
+
+    val a: String? = null
+    val vouSair: Boolean = if (a == "sol") true else false
+    println(vouSair)
+
+    fun teste(valor: Any) {  //safe cast
+        val numero: Int? = valor as? Int
+        println(numero)
+    }
+
+    teste("a")
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,50 +1,12 @@
-import br.com.exception.SaldoInsuficienteException
-import br.com.modelo.*
-import br.com.teste.testeAny
-import br.com.teste.testeExpressao
-import br.com.teste.testeObjects
-import java.util.Objects
-
-fun funcao2() {
-    println("inicio funcao 2")
-    for (i in 1..5) {
-        println(i)
-    }
-    val endereco = Any()
-    try {
-        endereco as Endereco
-    } catch (e: SaldoInsuficienteException) {
-        println(e.printStackTrace())
-        throw SaldoInsuficienteException("Saldo insuficiente")
-    } catch (e: Exception) {
-        println("exception genérica capturada")
-    }
-
-    println("fim função 2 ")
-}
-
-fun funcao1() {
-    println("inicio funcao 1")
-    funcao2()
-    println("fim funcao1")
-}
+import br.com.modelo.Endereco
+import java.util.*
 
 
 fun main() {
-    //testeCondicoes(saldo)
-    println("meu banco")
-    //testeCopiaRefeencia()
-    testeComportamentoFuncao()
-    //testeFuncionario()
-    testeAutenticacao()
-    //testeObjects()
-    //testeAny()
-    //testeExpressao()
-    //funcao1()
-
 
 
 }
+
 
 
 
